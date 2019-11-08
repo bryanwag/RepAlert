b8950a270c8fa7e56c6dea2394703054917a1270
Bryan <bryananono@gmail.com>
2019-11-08 12:22:26 -0500

    next spam num bug fix

--- update_official_rep_delegator_status_5219.py
+++ update_official_rep_delegator_status_5219.py
@@ -7,7 +7,7 @@ Created on Tue Feb  5 23:31:56 2019
 
 
 """
-import pandas as pd,math,numpy as np,json,requests
+import pandas as pd,math,numpy as np,json,requests,random
 
 #set votes threshold (in NANO) to filter delegators
 #votes_threshold=10000
@@ -85,12 +85,12 @@ def get_delegators_status_df(delegators_df,badrep,votes_threshold):
             #if there is no successful change, we need the rest of info to determine how to send txns
             if delegators.loc[addy,'success']==0:
                 #we need to increase txns for high transaction number accounts
-                if math.log(transaction_number)<2:
+                if transaction_number<100:
                     #the floor value is 2
                     delegators.loc[addy,'next_spam_number']=2
                 else:
                     #natural log of transaction number
-                    delegators.loc[addy,'next_spam_number']=round(math.log(transaction_number))
+                    delegators.loc[addy,'next_spam_number']=random.randint(2,5)
                     
                 #we check whether the account has pending txns from our addresses
                 url='https://api.nanocrawler.cc/v2/accounts/'+addy+'/pending'
